bf16fb1e521674e3f67876ae621efb066c9da1ce
Kirti Aggarwal <kirti057btcse21@igdtuw.ac.in>
2023-02-11 09:01:11 -0800

    store

--- client/src/store.js
+++ client/src/store.js
@@ -2,7 +2,7 @@ import {combineReducers,applyMiddleware} from "redux";
 import thunk from "redux-thunk";
 import {composeWithDevTools} from "redux-devtools-extension";
 import {configureStore} from "@reduxjs/toolkit";
-import productReducer from "./reducers/productReducer";
+
 const reducer=combineReducers({});
 let initialState={};
 const middleware=[thunk];
